9a3f6d6d9b30e84e88222913ebe7eb7b920aa618
Edward Fox <edwardkenfox@gmail.com>
2017-12-16 19:12:20 +0900

    Use external URL source

--- capture.js
+++ capture.js
@@ -2,20 +2,26 @@
 
 const puppeteer = require('puppeteer');
 const mkdirp = require('mkdirp');
+const fs = require('fs')
+const { URL } = require('url')
 
 async function main() {
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   let page = await browser.newPage();
 
   page.setViewport({ width: 1280, height: 1080 });
-  // await page.goto(`file://${__dirname}/index.html`);
-  await page.goto(`https://staging.repro.io/tool/push_notification/`);
-  await new Promise(res => setTimeout(() => res(), 300));
-  await page.screenshot({ path: 'screenshot/index.png' });
 
-  await page.goto(`https://staging.repro.io/tool/user_recordings/`);
-  await new Promise(res => setTimeout(() => res(), 300));
-  await page.screenshot({ path: 'screenshot/index2.png' });
+  const json = fs.readFileSync('./screenshots.json')
+  const rawUrls = JSON.parse(json)["screenshot_urls"]
+  const urls = rawUrls.map(url => new URL(url))
+
+  await (
+    urls.map(async (url) => {
+      await page.goto(url.href);
+      await new Promise(res => setTimeout(() => res(), 300));
+      await page.screenshot({ path: `screenshot/${url.pathname.replace(/\//g, '_')}.png` });
+    })
+  )();
 
   await page.close();
   await browser.close();
