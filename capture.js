9fcbd3454d1bdd6a93382af7333b176e2e24f3dd
Edward Fox <edwardkenfox@gmail.com>
2017-12-16 20:38:26 +0900

    Update script

--- capture.js
+++ capture.js
@@ -7,23 +7,25 @@ const { URL } = require('url')
 
 async function main() {
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-  let page = await browser.newPage();
-
-  page.setViewport({ width: 1280, height: 1080 });
 
   const json = fs.readFileSync('./screenshots.json')
   const rawUrls = JSON.parse(json)["screenshot_urls"]
   const urls = rawUrls.map(url => new URL(url))
 
   await (
-    urls.map(async (url) => {
-      await page.goto(url.href);
-      await new Promise(res => setTimeout(() => res(), 300));
-      await page.screenshot({ path: `screenshot/${url.pathname.replace(/\//g, '_')}.png` });
+    new Promise(resolve => {
+      urls.map(async url => {
+        let page = await browser.newPage();
+        await page.setViewport({ width: 1280, height: 1080 });
+        await page.goto(url.href);
+        await new Promise(res => setTimeout(() => res(), 300));
+        await page.screenshot({ path: `screenshot/${url.pathname.replace(/\//g, '_')}.png` });
+        await page.close();
+      })
+      resolve();
     })
   )();
 
-  await page.close();
   await browser.close();
 }
 
